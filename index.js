ec888405a00dcd98f7c5bbc8d79d1d79f1c01746
Glynn Phillips <glynn.phillips@ft.com>
2024-02-26 17:21:45 +0000

    Convert to function

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const fakeResponse = () => 'https://www.ft.com/__assets/creatives/js/blank.js';
+
 module.exports = {
-    core: '',
-    enhanced: ''
+    core: fakeResponse,
+    enhanced: fakeResponse
 }
